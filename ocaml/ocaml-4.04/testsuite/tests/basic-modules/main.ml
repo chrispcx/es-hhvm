dd3e7910ed6dcd00b858a23a955e52fe44efbcc8
peiqiang <peiqiang@huawei.com>
2017-02-06 10:53:42 +0800

    upload ocaml package

---
+++ ocaml/ocaml-4.04/testsuite/tests/basic-modules/main.ml
@@ -0,0 +1,22 @@
+(* PR#6435 *)
+
+module F (M : sig
+           type t
+           module Set : Set.S with type elt = t
+         end) =
+struct
+ let test set = Printf.printf "%d\n" (M.Set.cardinal set)
+end
+
+module M = F (Offset)
+
+let () = M.test (Offset.M.Set.singleton "42")
+let v = Pr6726.Test.v
+
+(* PR#7427 *)
+
+let () =
+  try
+    let module M = Pr7427.F () in
+    failwith "Test failed"
+  with Assert_failure _ -> ()
